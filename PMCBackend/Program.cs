d64b5f8b03136651df4e4e7c1d840d7fca0bde77
shin yamamoto <shin_yamamoto@support-s-frontier.com>
2021-07-12 21:15:41 +0900

    コンフィグから設定を取得

--- PMCBackend/Program.cs
+++ PMCBackend/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PMCBackend.DropBox;
+using System;
 
 namespace PMCBackend
 {
@@ -6,7 +7,25 @@ namespace PMCBackend
     {
         public static void Main(string[] args)
         {
+            Run();
             _ = Console.ReadLine();
+        }
+
+        public static async void Run()
+        {
+            var appKey = Properties.Settings.Default.AppKey;
+            var appSecret = Properties.Settings.Default.AppSecret;
+            var accessToken = Properties.Settings.Default.AccessToken;
+            var dropBoxClient = new DropBoxClient(appKey, appSecret).SetAccessToken(accessToken);
+            try
+            {
+                var file = await dropBoxClient.ListFolder();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
         }
     }
 }
